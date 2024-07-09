224306354e0a2765dbd42f92a3147a3b5d6d2b34
Abdullah Saied <abdallasaid201@gmail.com>
2024-07-09 22:40:52 +0300

    solve fizz buzz problem

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
 //Input: n = 20
 //Output: [1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz]
 
-//dfasdfsfd
-//new line
-//sdajdfsdf
-//sddfsaf
+//input
+$number=readline('enter the number: ');
+if($number%3==0 && $number%5==0){
+    echo "FizzBuzz";
+}else if ($number%3==0) {
+    echo "Fizz";
+}else if ($number%5==0) {
+    echo "Buzz";
+}else {
+    echo $number;
+}
+$a=10;
+$b=20;
+//a=a+b;//a=30 (10+20)
+//b=a-b;//b=10 (30-20)
+//a=a-b;//a=20 (30-10)
+
