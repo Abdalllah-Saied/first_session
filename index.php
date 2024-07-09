dcc0bd9a0db2f346bae425fe8335f2c55b2f082b
Abdullah Saied <abdallasaid201@gmail.com>
2024-07-09 21:16:38 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//Input: n = 3
+//Output: [1 2 Fizz]
+//
+//Input: n = 10
+//Output: [1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz]
+//
+//Input: n = 20
+//Output: [1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz]
+
